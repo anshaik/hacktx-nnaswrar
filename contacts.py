f6efb525ba5f00377e6915d357d584df3f255e12
Adil Shaikh <adil.shaikh@sabre.com>
2014-10-18 20:04:53 -0500

    adding contacts; uses context.io

---
+++ contacts.py
@@ -0,0 +1,9 @@
+import contextio as c
+
+CONSUMER_KEY = 'YOUR_API_KEY'
+CONSUMER_SECRET = 'YOUR_API_SECRET'
+
+context_io = c.ContextIO(
+    consumer_key=CONSUMER_KEY, 
+    consumer_secret=CONSUMER_SECRET
+)
