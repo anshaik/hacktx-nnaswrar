4d4f26be64a109ab8106edf7a2118e6cdc4a00b3
Adil Shaikh <adil.shaikh@sabre.com>
2014-10-18 20:07:02 -0500

    adding key and secret

--- contacts.py
+++ contacts.py
@@ -1,7 +1,7 @@
 import contextio as c
 
-CONSUMER_KEY = 'YOUR_API_KEY'
-CONSUMER_SECRET = 'YOUR_API_SECRET'
+CONSUMER_KEY = 'hacktx-2014-nasawar@outlook.com'
+CONSUMER_SECRET = '54430b38b9f7091255e9affb'
 
 context_io = c.ContextIO(
     consumer_key=CONSUMER_KEY, 
