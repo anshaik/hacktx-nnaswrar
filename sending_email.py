400b2c27eeccb59d6a73436c910127140c91057a
Adil Shaikh <adil.shaikh@sabre.com>
2014-10-19 02:26:53 -0500

    adding some files and updating sending_email.py

--- sending_email.py
+++ sending_email.py
@@ -1,11 +1,20 @@
 import sendgrid
+import json
 
-sg = sendgrid.SendGridClient('hacktx-nasawarriors', 'hacktxpassword')
+with open('top_contacts.json') as json_data:
+    d = json.load(json_data)
+    json_data.close()
+    temp = d.get('matches')
+    
+    emails = (temp[0]['email'], temp[1]['email'], temp[2]['email'], temp[3]['email'])
 
-message = sendgrid.Mail()
-message.add_to('Adil Shaikh <anshaik@gmail.com>')
-message.set_subject('Example')
-message.set_html('Body')
-message.set_text('There is some Body text here that we will change or whatever')
-message.set_from('Nasa-Warriors <adil@nasawar.com>')
-status, msg = sg.send(message)
+    sg = sendgrid.SendGridClient('hacktx-nasawarriors', 'hacktxpassword')
+
+    for email in emails:
+		message = sendgrid.Mail()
+		message.add_to(email)
+		message.set_subject('Example')
+		message.set_html('Body')
+		message.set_text('There is some Body text here that we will change or whatever')
+		message.set_from('Nasa-Warriors <team@nasawar.com>')
+		status, msg = sg.send(message)
